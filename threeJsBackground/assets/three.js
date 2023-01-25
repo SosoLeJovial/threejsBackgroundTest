04ec300cfd76fa4242b8a542eb70d1084bdb9946
SOGtech <contactpro.sgtech@gmail.com>
2023-01-25 21:35:21 +0100

    remove model 3d (test fonctionnality) creat function to add stars to bg

--- threeJsBackground/assets/three.js
+++ threeJsBackground/assets/three.js
@@ -1,39 +1,44 @@
 import * as THREE from 'three';
+import { PointLight } from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader }   from 'three/addons/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-camera.position.set( 0, 0, 1 );
+camera.position.set( 0, 0, 10 );
 camera.lookAt( 0, 0, 0 );
 
 
-const loader = new GLTFLoader();
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+    star.position.set(x,y,z);
+    scene.add(star);
+}
+Array(400).fill().forEach(addStar);
 
-loader.load( './dist/model3d/sonicOrmario/scene.gltf', function ( gltf ) {
-
-	scene.add( gltf.scene );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-} );
-
-
-
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20,20,20);
+const ambiantLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambiantLight)
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
-    camera.position.z = 100;
+    camera.position.z = 30;
     camera.lookAt(0,0,0);
+    controls.update();
 }
 animate();
 
